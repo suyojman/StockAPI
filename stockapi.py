f76d9f58bf0b8d49b56ff53e55242ac5d8da6308
suyoj man <suyojman@gmail.com>
2019-04-22 21:44:21 +0545

    Created a module to display graph

--- stockapi.py
+++ stockapi.py
@@ -3,12 +3,17 @@ import pprint
 import json
 import csv
 import time
+import datetime as dt
 from config import API_KEY
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas_datareader as web
+
 
 a = input("Enter the symbol !! ")
 symbol = a.upper()
-how_many_days = input("Enter how many days from today !!")
-response = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+symbol+'&apikey=' + API_KEY)
+response = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol='+symbol+'&apikey=' + API_KEY)
 
 
 if response.status_code == 200:
@@ -16,13 +21,9 @@ if response.status_code == 200:
     day = []
     a=0
     for i in result['Time Series (Daily)'].keys():
-        a += 1
-        if a > int(how_many_days):
-            break
         day.append(i)
-   
-print("Preparing CSV....")
 
+print("Preparing CSV....")
 
 myData = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
 with open('stock_info_'+symbol+ '.csv', mode='w') as csvFile:
@@ -32,4 +33,17 @@ with open('stock_info_'+symbol+ '.csv', mode='w') as csvFile:
         writer.writerow([j, result["Time Series (Daily)"][j]["1. open"], result["Time Series (Daily)"][j]["2. high"], result["Time Series (Daily)"][j]["3. low"], result["Time Series (Daily)"][j]["4. close"], result["Time Series (Daily)"][j]["5. volume"]])
 csvFile.close()
 time.sleep(2)
+
+print("Preparing Graph!!")
+
+#Plotting the graph
+
+style.use("ggplot")
+start = dt.datetime(2002,1,1)
+end = dt.datetime.now()
+print(end)
+df = web.get_data_yahoo(symbol, start, end)
+df["Adj Close"].plot()
+plt.show()
+
 print("DONE !!")
